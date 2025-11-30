dc29b507d318390168e978a3329e0c40568912e6
Nick Garvey <garvey.nick@gmail.com>
2025-11-29 19:59:19 -0800

    Initial commit with current deployment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+
+{
+	description = "K8s GitOps development environment";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+			in {
+				devShells = {
+					default = pkgs.mkShell {
+					buildInputs = [
+						pkgs.kubectl
+					];
+            shellHook = ''
+            export PS1="[k8s-gitops] \[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+            '';
+					};
+				};
+			}
+		);
+}
